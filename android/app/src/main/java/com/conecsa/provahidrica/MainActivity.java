abfaac24bdecd7a0876af9abb54b960ec04d7c75
Cristiano Leite <cristianosleite@hotmail.com>
2025-01-23 12:00:22 -0300

    change package name

--- android/app/src/main/java/com/conecsa/provahidrica/MainActivity.java
+++ android/app/src/main/java/com/conecsa/provahidrica/MainActivity.java
@@ -1,18 +1,27 @@
 package com.conecsa.provahidrica;
 
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.JSObject;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginResult;
+
 import android.os.Bundle;
+
 import androidx.core.app.ActivityCompat;
+
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.widget.Toast;
 
+
 public class MainActivity extends BridgeActivity {
   private static final int REQUEST_WRITE_STORAGE = 112;
-  private static final int REQUEST_BLUETOOTH = 113;
+  private static final int REQUEST_BLUETOOTH = 114;
+  private static final String TAG = "MainActivity";
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
+
     super.onCreate(savedInstanceState);
 
     if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
