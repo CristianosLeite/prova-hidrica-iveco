622d9195270795ae04ff0333fcc964f436d8eb0e
Cristiano Leite <cristianosleite@hotmail.com>
2025-03-08 20:46:55 -0300

    refactor: improve error handling and add documentation in recipes controller

--- src/api/controllers/recipes.controller.js
+++ src/api/controllers/recipes.controller.js
@@ -1,28 +1,40 @@
 const { Router } = require("express");
 const Recipe = require("../models/recipe.model.js");
-const { Op } = require("sequelize");
 
+/**
+ * Recipes controller
+ */
 class RecipeController {
   constructor(io) {
     this.io = io;
     this.router = Router();
     this.router.post("/create", this.create.bind(this));
     this.router.get("/all", this.all.bind(this));
-    this.router.get("/between", this.between.bind(this));
     this.router.get("/one", this.retrieve.bind(this));
     this.router.put("/update", this.update.bind(this));
     this.router.delete("/delete", this.delete.bind(this));
   }
 
+  /**
+   * Get the router
+   * @returns { Router }
+   */
   getRouter() {
     return this.router;
   }
 
+  /**
+   * Create a new recipe in the database
+   * @param {*} req
+   * @param {*} res
+   * @returns { Recipe }
+   * @example /api/recipe/create
+   */
   async create(req, res) {
     const newRecipe = req.body;
 
     if (!newRecipe) {
-      res.status(400).send("Missing recipe");
+      res.status(400).send({ message: "Missing recipe" });
       return;
     }
 
@@ -33,66 +45,88 @@ class RecipeController {
     });
   }
 
+  /**
+   * Retrieve all recipes from the database
+   * @param {*} req
+   * @param {*} res
+   * @returns { Recipe[] }
+   * @example /api/recipe/all
+   */
   async all(req, res) {
     await Recipe.findAll().then((recipes) => {
       res.json(recipes);
     });
   }
 
-  async between(req, res) {
-    await Recipe.findAll({
-      where: {
-        recipe_id: {
-          [Op.between]: [req.query.start, req.query.end],
-        },
-      },
-    }).then((recipes) => {
-      res.json(recipes);
-    });
-  }
-
+  /**
+   * Retrieve a recipe by its vp
+   * @param {*} req
+   * @param {*} res
+   * @returns { Recipe }
+   * @example /api/recipe/one?vp=123456
+   */
   async retrieve(req, res) {
     const { vp } = req.query;
 
     if (!vp) {
-      res.status(400).send("Missing vp");
+      res.status(400).send({ message: "Missing vp" });
       return;
     }
 
     await Recipe.findOne({ where: { vp } }).then((recipe) => {
       if (!recipe) {
-        res.status(404).send("Recipe not found");
+        res.status(404).send({ message: "Recipe not found" });
       } else {
         res.json(recipe);
       }
     });
   }
 
+  /**
+   * Update a recipe in the database
+   * @param {*} req
+   * @param {*} res
+   * @returns { Recipe }
+   * @example /api/recipe/update?recipe_id=1
+   */
   async update(req, res) {
+    const { recipe_id } = req.query;
     const updatedRecipe = req.body;
 
+    if (!recipe_id) {
+      res.status(400).send({ message: "Missing recipe ID" });
+      return;
+    }
+
     if (!updatedRecipe) {
-      res.status(400).send("Missing updated recipe");
+      res.status(400).send({ message: "Missing updated recipe" });
       return;
     }
 
     await Recipe.update(updatedRecipe, {
-      where: { recipe_id: updatedRecipe.recipe_id },
+      where: { recipe_id: recipe_id },
     }).then(() => {
       res.json(updatedRecipe);
     });
   }
 
+  /**
+   * Delete a recipe from the database
+   * @param {*} req
+   * @param {*} res
+   * @returns { json } message - Recipe deleted
+   * @example /api/recipe/delete?recipe_id=1
+   */
   async delete(req, res) {
     const { recipe_id } = req.query;
 
     if (!recipe_id) {
-      res.status(400).send("Missing recipe ID");
+      res.status(400).send({ message: "Missing recipe ID" });
       return;
     }
 
     await Recipe.destroy({ where: { recipe_id } }).then(() => {
-      res.json({ recipe_id });
+      res.json({ message: "Recipe deleted" });
     });
   }
 }
