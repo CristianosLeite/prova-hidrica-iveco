b5356b004cfe26c9a7898c725b01492c5cc44958
Cristiano Leite <cristianosleite@hotmail.com>
2025-04-03 10:05:51 -0300

    feat: enhance ApiService with detailed method documentation and new socket event handlers

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -6,18 +6,37 @@ import { SocketResponse } from 'src/app/types/socketResponse.type';
 import { StorageService } from '../storage/storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Recipe } from 'src/app/types/recipe.type';
-import { Device } from 'src/app/types/device.type';
+import { Device, Platform } from 'src/app/types/device.type';
 
+/**
+ * * ApiService is responsible for managing the connection to the API service using Socket.IO.
+ * * It handles events such as connection, disconnection, and background service initialization.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  /**
+   * * Socket.IO client instance.
+   */
   private socket: Socket | undefined;
+  /**
+   * * Server connection parameters.
+   */
   private protocol = '';
   private serverIp = '';
   private serverPort = '';
+  /**
+   * * Server URL.
+   */
   private url = '';
+  /**
+   * * Event emitter for API connection status.
+   */
   @Output() public isApiConnected: EventEmitter<boolean> = new EventEmitter();
+  /**
+   * * Event emitter for background service initialization status.
+   */
   @Output() public isBackgroundServiceInitialized: EventEmitter<boolean> = new EventEmitter();
 
   constructor(
@@ -25,19 +44,26 @@ export class ApiService {
     private authService: AuthService,
   ) { }
 
+  /**
+   * * Initializes the API service by setting up the Socket.IO connection and event listeners.
+   * * It retrieves server connection parameters from storage and connects to the server.
+   */
   public async init() {
     try {
+      // Get server connection parameters from storage
       this.storage.storageCreated.subscribe(async () => {
         Promise.all([
           await this.storage.get('serverProtocol'),
           await this.storage.get('serverIp'),
           await this.storage.get('serverPort')
         ]).then(([protocol, serverIp, serverPort]) => {
+          // Set default values if not provided
           this.protocol = protocol || 'https';
           this.serverIp = serverIp || '172.29.96.2';
           this.serverPort = serverPort || '4000';
           this.url = `${this.protocol}://${this.serverIp}:${this.serverPort}`;
 
+          // Initialize Socket.IO connection
           this.socket = io(this.url, {
             transports: ['websocket'],
             withCredentials: true,
@@ -50,6 +76,10 @@ export class ApiService {
 
           this.socket.on('backgroundServiceInitialized', () => {
             this.isBackgroundServiceInitialized.emit(true);
+          });
+
+          this.socket.on("unauthenticated", () => {
+            this.authService.logOut();
           });
 
           this.socket.on('disconnect', () => {
@@ -65,6 +95,11 @@ export class ApiService {
     console.log('API service initialized');
   }
 
+  /**
+   * * Connects to the background service and emits an event to start it.
+   * * It also handles the response from the background service.
+   * * @returns A promise that resolves with the socket response.
+   */
   public connectBackgroundService(): Promise<SocketResponse> {
     return new Promise((resolve, reject) => {
       this.socket?.emit('startBackgroundService');
@@ -78,9 +113,15 @@ export class ApiService {
     });
   }
 
-  public authenticate(): Promise<SocketResponse> {
+  /**
+   * * Authenticates the user by emitting an authentication event and handling the response.
+   * * It also sets the logged-in user in the AuthService.
+   * * @param sendMessage - Optional parameter to control whether to send the authentication message.
+   * * @returns A promise that resolves with the socket response.
+   */
+  public listenAuthentication(sendMessage: boolean = true): Promise<SocketResponse> {
     return new Promise((resolve, reject) => {
-      this.socket?.emit('authenticate');
+      if (sendMessage) this.socket?.emit('authenticate')
       this.socket?.on('authenticated', (user: User) => {
         resolve({ type: 'success', payload: { message: 'Authenticated successfully', user } });
         this.authService.setLoggedUser(user);
@@ -91,6 +132,31 @@ export class ApiService {
     });
   }
 
+  /**
+   * Unauthenticate the user by emitting an unauthentication event and handling the response.
+   * * It also logs out the user from the AuthService.
+   * @param sendMessage - Optional parameter to control whether to send the unauthentication message.
+   * @returns
+   */
+  public listenUnAuthentication(sendMessage: boolean = true): Promise<SocketResponse> {
+    return new Promise((resolve, reject) => {
+      if (sendMessage) this.socket?.emit('unAuthenticate')
+      this.socket?.on('unAuthenticated', () => {
+        resolve({ type: 'success', payload: { message: 'Unauthenticated successfully' } });
+        this.authService.logOut();
+      });
+      this.socket?.on('error', (error: any) => {
+        reject({ type: 'error', payload: { message: 'Unauthentication failed', error } });
+      });
+    });
+  }
+
+  /**
+   * * Creates a new user by emitting a user creation event and handling the response.
+   * * It also sets the logged-in user in the AuthService.
+   * @param user - The user object to be created.
+   * @returns A promise that resolves with the socket response.
+   */
   public createUser(user: User): Promise<SocketResponse> {
     return new Promise((resolve, reject) => {
       this.socket?.emit('createUser', user);
@@ -103,7 +169,14 @@ export class ApiService {
     });
   }
 
-  public verficationCompleted(testId: string, infiltrationTest: InfiltrationTest): Promise<SocketResponse> {
+  /**
+   * * Signals that a check has been completed by the operator.
+   * * It emits an event to the server and also updates the local storage with the test result when it comes from another device.
+   * @param testId
+   * @param infiltrationTest
+   * @returns
+   */
+  public verficationCompleted(infiltrationTest: InfiltrationTest): Promise<SocketResponse> {
     return new Promise((resolve, reject) => {
       this.socket?.emit('verificationCompleted', infiltrationTest);
       this.socket?.on('verificationCompleted', async (infiltrationTest: InfiltrationTest) => {
@@ -116,6 +189,10 @@ export class ApiService {
     });
   }
 
+  /**
+   * Handles the barcode reader events and recipe provided by the server.
+   * @returns A promise that resolves with the socket response.
+   */
   public BarcodeReader(): Promise<SocketResponse> {
     return new Promise((resolve, reject) => {
       this.socket?.on('barcodeData', (data: string) => {
@@ -131,6 +208,11 @@ export class ApiService {
     });
   }
 
+  /**
+   * Sends the barcode data to the server and handles the response.
+   * @param barcode - The barcode data to be sent.
+   * @returns A promise that resolves with the socket response.
+   */
   public sendBarcodeData(barcode: string): Promise<SocketResponse> {
     return new Promise((resolve, reject) => {
       this.socket?.emit('sendingBarcode', barcode);
@@ -144,6 +226,10 @@ export class ApiService {
     });
   }
 
+  /**
+   * * Enables the operation by emitting an event to the server and handling the response.
+   * @returns A promise that resolves with the socket response.
+   */
   public enableOperation(): Promise<SocketResponse> {
     return new Promise((resolve, reject) => {
       this.socket?.emit('enableOperation');
@@ -157,6 +243,10 @@ export class ApiService {
     });
   }
 
+  /**
+   * * Reset alarms and start operation again.
+   * @returns A promise that resolves with the socket response.
+   */
   public resetOperation(): Promise<SocketResponse> {
     return new Promise((resolve, reject) => {
       this.socket?.emit('resetOperation');
@@ -170,10 +260,14 @@ export class ApiService {
     });
   }
 
+  /**
+   * * Stops the operation by emitting an event to the server and handling the response.
+   * @returns A promise that resolves with the socket response.
+   */
   public stopOperation(): Promise<SocketResponse> {
     return new Promise((resolve, reject) => {
       this.socket?.emit('stopOperation');
-      this.socket?.on('operationStopped', () => {
+      this.socket?.on('operationStopped', async () => {
         resolve({ type: 'success', payload: { message: 'Operation stopped successfully' } });
       });
       this.socket?.on('error', (error: any) => {
@@ -183,6 +277,11 @@ export class ApiService {
     });
   }
 
+  /**
+   * * Sends device data info to the server and handles the response.
+   * @param device - The device object to be sent.
+   * @returns A promise that resolves with the socket response.
+   */
   public sendDeviceData(device: Device): Promise<SocketResponse> {
     return new Promise((resolve, reject) => {
       this.socket?.emit('sendDeviceData', device);
@@ -191,6 +290,38 @@ export class ApiService {
       });
       this.socket?.on('error', (error: any) => {
         reject({ type: 'error', payload: { message: 'Device data send failed', error } });
+      });
+    });
+  }
+
+  /**
+   * * Send a command to open the pick-by-open door and handle the response.
+   * @returns A promise that resolves with the socket response.
+   */
+  public openDoor(): Promise<SocketResponse> {
+    return new Promise((resolve, reject) => {
+      this.socket?.emit('openDoor');
+      this.socket?.on('doorOpened', () => {
+        resolve({ type: 'success', payload: { message: 'Door opened successfully' } });
+      });
+      this.socket?.on('error', (error: any) => {
+        reject({ type: 'error', payload: { message: 'Door opening failed', error } });
+      });
+    });
+  }
+
+  /**
+   * Handles the door closed event and logs out the user.
+   * @returns A promise that resolves with the socket response.
+   */
+  public doorClosed(): Promise<SocketResponse> {
+    return new Promise((resolve, reject) => {
+      this.socket?.on('doorClosed', () => {
+        resolve({ type: 'success', payload: { message: 'Door closed successfully' } });
+        this.authService.logOut();
+      });
+      this.socket?.on('error', (error: any) => {
+        reject({ type: 'error', payload: { message: 'Door closing failed', error } });
       });
     });
   }
