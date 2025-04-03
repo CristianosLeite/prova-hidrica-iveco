b122329382ca7ecd010ee05a366a01e8f8dfdb2b
Cristiano Leite <cristianosleite@hotmail.com>
2025-04-03 10:01:46 -0300

    feat: implement door control logic with open/close events and state management

--- src/api/sockets/background/background.socket.js
+++ src/api/sockets/background/background.socket.js
@@ -1,3 +1,6 @@
+let currentDoor = 1;
+let openCount = 0;
+
 module.exports = function (io, socket) {
   socket.on("startBackgroundService", () => {
     io.emit("startBackgroundService");
@@ -6,5 +9,24 @@ module.exports = function (io, socket) {
   socket.on("backgroundServiceInitialized", (data) => {
     io.emit("backgroundServiceInitialized", data);
   });
-};
 
+  socket.on("openDoor", () => {
+    io.emit("openDoor", currentDoor);
+  });
+
+  socket.on("doorOpened", (data) => {
+    if (data === currentDoor) {
+      openCount++; // Increase the counter when the door is opened
+      if (openCount >= 2) {
+        // If the door is opened twice, switch to the other door
+        currentDoor = currentDoor === 1 ? 2 : 1;
+        openCount = 0; // Reset the counter
+      }
+    }
+    io.emit("doorOpened", data);
+  });
+
+  socket.on("doorClosed", (data) => {
+    io.emit("doorClosed", data);
+  });
+};
